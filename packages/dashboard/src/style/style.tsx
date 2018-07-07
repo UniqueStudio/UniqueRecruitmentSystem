990ce41bcbd01238a5790cc5eaa62e41762f090a
winderica <winderica@gmail.com>
2018-07-07 23:11:10 +0800

    Built environment && simple start

---
+++ packages/dashboard/src/style/style.tsx
@@ -0,0 +1,16 @@
+import { StyleRulesCallback } from '@material-ui/core/styles/withStyles';
+
+const styles: StyleRulesCallback = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+});
+
+export default styles;
