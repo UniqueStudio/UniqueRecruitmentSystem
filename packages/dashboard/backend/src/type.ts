5dd29161f5faf8b945582cf9d84a56ab11f2ec80
winderica <winderica@gmail.com>
2018-07-17 21:58:44 +0800

    Add: type definition for candidate, recruitment and user

--- packages/dashboard/backend/src/type.ts
+++ packages/dashboard/backend/src/type.ts
@@ -5,8 +5,42 @@ export interface Candidate {
     name: string;
     grade: string;
     institute: string;
+    major: string;
+    score: "10%" | "25%" | "50%" | "100%";
+    mail: string;
+    phone: string;
     abandon?: boolean;
     group: string;
+    sex: "Male" | "Female";
     step: string;
+    intro: string;
+    resume: string; // file path
     comments: object;
+}
+
+type group = "web" | "lab" | "ai" | "game" | "android" | "ios" | "design" | "pm";
+
+interface Data {
+    group: group;
+    total: number;
+    steps: number[];
+}
+
+export interface Recruitment {
+    _id: ObjectID;
+    title: string; // e.g. 2018A || 2018S (A: AUTUMN, S: SPRING, C: CAMP)
+    total: number;
+    data: Data[];
+}
+
+export interface User {
+    _id: ObjectID;
+    name: string;
+    joinTime: string; // e.g. 2018A || 2018S (A: AUTUMN, S: SPRING, C: CAMP)
+    isCaptain: boolean;
+    isAdmin: boolean;
+    phone: string;
+    mail: string;
+    sex: "Male" | "Female";
+    group: group;
 }
