a3df575f48a678c1d1bde83e3e873be7b9e3d358
winderica <winderica@gmail.com>
2018-08-02 19:02:30 +0800

    Add: login with WeChat

--- packages/dashboard/src/lib/const.ts
+++ packages/dashboard/src/lib/const.ts
@@ -1,6 +1,7 @@
 export const GROUP = ['Web', 'Lab', 'AI', 'Game', 'Android', 'iOS', 'Design', 'PM'];
 export const STEP = ['报名流程', '笔试流程', '面试流程', '熬测流程', '群面流程', '通过'];
 export const URL = 'http://39.108.175.151:5000';
+export const QR_CODE_URL = 'https://open.work.weixin.qq.com/wwopen/sso/qrImg?key=';
 
 export interface Candidate {
     cid: string;
