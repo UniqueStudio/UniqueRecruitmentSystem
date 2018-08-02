fd2d3c209c7d9851fa8205d36c16656e734b2db1
winderica <winderica@gmail.com>
2018-08-02 19:01:40 +0800

    Implement WeChat login

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -51,8 +51,11 @@ const verifyJWT = (token?: string) => {
     })
 };
 
-const getQRCodeURL = 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid=ww6879e683e04c1e57&agentid=1000011&redirect_uri=https%3A%2F%2Fopen.hustunique.com%2Fauth&state=api';
+const getQRCodeURL = 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid=ww6879e683e04c1e57&agentid=1000014&redirect_uri=https%3A%2F%2Fopen.hustunique.com%2Fauth&state=api';
 const scanningURL = 'https://open.work.weixin.qq.com/wwopen/sso/l/qrConnect?key=';
+const accessTokenURL = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww6879e683e04c1e57&corpsecret=aZm2TSKTl-qGFRDtcyXHrR4HUUU_eRIyxMCqLyyO184';
+const userIDURL = (accessToken: string, code: string) => `https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=${accessToken}&code=${code}`;
+const userInfoURL = (accessToken: string, uid: string) => `https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=${accessToken}&userid=${uid}`;
 
 app.use(bodyParser.json({
     limit: '1mb'
@@ -95,10 +98,19 @@ app.get('/user/:key/status', (req, res) => {
             const scanResult = await scanResponse.text();
             const status = JSON.parse(scanResult.match(/{.+}/)![0]).status;
             //if (status === 'QRCODE_SCAN_SUCC') {
-                const loginResponse = await fetch(`${scanningURL}${req.params.key}&lastStatus=${status}`);
-                const loginResult = await loginResponse.text();
-                const auth_code = JSON.parse(loginResult.match(/{.+}/)![0]).auth_code;
-                res.send({ code: auth_code, type: 'success' })
+            const loginResponse = await fetch(`${scanningURL}${req.params.key}&lastStatus=${status}`);
+            const loginResult = await loginResponse.text();
+            const auth_code = JSON.parse(loginResult.match(/{.+}/)![0]).auth_code;
+            const accessTokenResponse = await fetch(accessTokenURL);
+            const accessTokenResult = await accessTokenResponse.json();
+            const accessToken = JSON.parse(accessTokenResult).access_token;
+            const userIDResponse = await fetch(userIDURL(accessToken, auth_code));
+            const userIDResult = await userIDResponse.json();
+            const uid = JSON.parse(userIDResult).UserId;
+            const userInfoResponse = await fetch(userInfoURL(accessToken, uid));
+            const userInfoResult = await userInfoResponse.json();
+            console.log(userInfoResult);
+            res.send({ code: auth_code, type: 'success' })
             //} else {
             //    res.send({ message: '登录超时，请重新登录', type: 'info' });
             //    return;
