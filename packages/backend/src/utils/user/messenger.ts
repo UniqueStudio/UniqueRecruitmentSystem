f591bacc137dd7a43586902a50cb3b45ce7ac08c
winderica <winderica@gmail.com>
2018-08-05 12:27:18 +0800

    Support image sending

--- packages/backend/src/utils/user/messenger.ts
+++ packages/backend/src/utils/user/messenger.ts
@@ -1,5 +1,7 @@
 import { Socket } from 'socket.io';
 
-export const messenger = (socket: Socket) => (name: string, avatar: string, time: string, message: string) => {
-    socket.broadcast.emit('receiveMessage', name, avatar, time, message);
+export const messenger = (socket: Socket) => (name: string, avatar: string, time: string, message: string, isImage: boolean) => {
+    isImage
+        ? socket.broadcast.emit('receiveImage', name, avatar, time, message)
+        : socket.broadcast.emit('receiveMessage', name, avatar, time, message);
 };
