202b0edf99dee91c81203153eb902d99bb7a5816
winderica <winderica@gmail.com>
2018-08-06 12:51:06 +0800

    Fix bug

--- packages/backend/src/utils/user/groupGetter.ts
+++ packages/backend/src/utils/user/groupGetter.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
 import { verifyJWT } from '../../lib/checker';
 import { database } from '../../app';
-import { ObjectId } from 'mongodb';
 
 export const groupGetter = (req: Request, res: Response) => {
     (async () => {
         try {
             verifyJWT(req.get('Authorization'));
-            const groupUsers = await database.query('users', { group: new ObjectId(req.params.group) });
+            const groupUsers = await database.query('users', { group: req.params.group });
             res.send({ data: groupUsers, type: 'success' });
         } catch (err) {
             res.send({ message: err.message, type: 'danger' });
