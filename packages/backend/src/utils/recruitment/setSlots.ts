fec5d9dc111581334aed492b17213241e67b1d43
winderica <winderica@gmail.com>
2018-08-07 18:27:55 +0800

    Try fix

--- packages/backend/src/utils/recruitment/setSlots.ts
+++ packages/backend/src/utils/recruitment/setSlots.ts
@@ -10,31 +10,36 @@ export const setSlots = (req: Request, res: Response) => {
             verifyJWT(req.get('Authorization'));
             const { group, slots, title } = req.body;
             let failed = 0;
-            let result;
             if (group !== 'interview') {
                 await database.update('recruitments', { title }, { [`time1.slots.${group}`]: slots });
                 const candidates = await database.query('candidates', { group, title, step: 2, abandon: false });
-                result = arrangeTime(slots, candidates, 1);
-                result.map(async i => {
+                const result = arrangeTime(slots, candidates, 1);
+                console.log(result);
+                const promises = result.map(async i => {
                     if (i['slot1']) {
                         await database.update('candidates', { _id: new ObjectId(i['_id']) }, { slot1: i['slot1'] })
                     } else {
                         failed++;
                     }
+                });
+                Promise.all(promises).then(() => {
+                    res.send({ type: 'success', result, failed });
                 })
             } else {
                 await database.update('recruitments', { title }, { slots });
                 const candidates = await database.query('candidates', { title, step: 4, abandon: false });
-                result = arrangeTime(slots, candidates, 2);
-                result.map(async i => {
+                const result = arrangeTime(slots, candidates, 2);
+                const promises = result.map(async i => {
                     if (i['slot2']) {
                         await database.update('candidates', { _id: new ObjectId(i['_id']) }, { slot2: i['slot2'] })
                     } else {
                         failed++;
                     }
+                });
+                Promise.all(promises).then(() => {
+                    res.send({ type: 'success', result, failed });
                 })
             }
-            res.send({ type: 'success', result, failed });
         } catch (err) {
             res.send({ message: err.message, type: 'danger' })
         }
