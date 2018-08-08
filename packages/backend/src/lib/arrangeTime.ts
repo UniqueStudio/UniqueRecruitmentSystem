850bf3693dedbc56232e819b12925add8e5b54ef
winderica <winderica@gmail.com>
2018-08-08 11:39:14 +0800

    Add interview time notification

--- packages/backend/src/lib/arrangeTime.ts
+++ packages/backend/src/lib/arrangeTime.ts
@@ -13,8 +13,8 @@ const converter = (selections: object[]) => {
 export const arrangeTime = (slots: number[], candidates: Candidate[], interview: 1 | 2) => {
 
     const time = {
-        morning: ['7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'],
-        afternoon: ['13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00'],
+        morning: ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'],
+        afternoon: ['13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00'],
         evening: ['18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00']
     };
 
@@ -57,7 +57,7 @@ export const arrangeTime = (slots: number[], candidates: Candidate[], interview:
                     i[`slot${interview}`] = [
                         i.date[~~(j / 3)],
                         ['morning', 'afternoon', 'evening'][j % 3],
-                        time[['morning', 'afternoon', 'evening'][j % 3]].splice(-1)[0] || ''
+                        time[['morning', 'afternoon', 'evening'][j % 3]].splice(0, 1)[0] || ''
                     ];
                     hasPlaced = true;
                 }
--- packages/backend/src/utils/sms/sendInterview.ts
+++ packages/backend/src/utils/sms/sendInterview.ts
@@ -1,5 +1,52 @@
 import { Request, Response } from 'express';
+import { verifyJWT } from '../../lib/checkData';
+import { database, getAsync, redisClient } from '../../app';
+import { smsSendURL, token } from '../../lib/consts';
+import { ObjectId } from 'mongodb';
+import fetch from 'node-fetch';
 
 export const sendInterview = (req: Request, res: Response) => {
-
+    const body = req.body;
+    const { step, candidates } = body;
+    (async () => {
+        try {
+            const decoded = verifyJWT(req.get('Authorization'));
+            const code = await getAsync(`userCode:${decoded['uid']}`);
+            if (body.code === code) {
+                const results = candidates.map(async (i: string) => {
+                    const candidateInfo = (await database.query('candidates', { _id: new ObjectId(i) }))[0];
+                    const slot = candidateInfo[`slot${step}`];
+                    const time = `${slot[0]}(${slot[1]})${slot[2]}`;
+                    let model = `${candidateInfo['name']}你好，你的${step === 2 ? '群面' : `${candidateInfo['group']}组组面`}时间为${time}，请准时到场`;
+                    const response = await fetch(smsSendURL, {
+                        method: 'POST',
+                        headers: {
+                            'Token': token,
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            phone: candidateInfo['phone'],
+                            template: 96387,
+                            param_list: [`${model}（请无视以下内容：`, '', '', '']
+                        })
+                    });
+                    const result = await response.json();
+                    if (result.code !== 200) {
+                        return candidateInfo['name'];
+                    }
+                });
+                Promise.all(results).then(failed => {
+                    const failedNames = failed.filter(i => i);
+                    failedNames.length === 0
+                        ? res.send({ type: 'success' })
+                        : res.send({ type: 'info', message: `未能成功发送短信的有：${failedNames}` })
+                });
+                redisClient.del(`userCode:${decoded['uid']}`);
+            } else {
+                res.send({ message: '验证码不正确', type: 'warning' })
+            }
+        } catch (err) {
+            res.send({ message: err.message, type: 'danger' })
+        }
+    })()
 };
