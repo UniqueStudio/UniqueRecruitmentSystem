5f85114551dc26f981596f22db083ed351d1da2a
winderica <winderica@gmail.com>
2018-08-09 11:10:51 +0800

    Optimize style

--- packages/form/src/component/Select/index.tsx
+++ packages/form/src/component/Select/index.tsx
@@ -29,18 +29,19 @@ class Select extends React.Component<Props> {
 
     public render() {
         const { selections, name } = this.props;
+        const { clicked, value } = this.state;
         return (
             <div className='selectContainer' onClick={this.handleClick}>
-                <div className='select'>
-                    <div className={classNames('selectValue', {'hidden': this.state.value === name})}>{this.state.value}</div>
-                    <div className={classNames({'hidden': this.state.value !== name})}>{name}</div>
+                <div className={classNames('select', {'selectClicked': clicked})}>
+                    <div className={classNames('selectValue', {'hidden': value === name})}>{value}</div>
+                    <div className={classNames({'hidden': value !== name})}>{name}</div>
                 </div>
-                <div className='selectMenu'>
+                {clicked && <div className='selectMenu'>
                     {selections.map((i, j) =>
                         <div key={j} onClick={this.handleChange}>{i}</div>
                     )}
-                </div>
-                <svg className='selectArrow'>
+                </div>}
+                <svg className={classNames('selectArrow', {'selectArrowRotate': clicked})}>
                     <polyline points="3 11, 15 23, 27 11"
                               strokeWidth="2"
                               stroke="#3FA9F5"
