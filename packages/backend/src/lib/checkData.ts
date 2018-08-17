0058e1059efef0fef4104be2c2d8efa92fe7dd11
winderica <winderica@gmail.com>
2018-08-17 13:14:00 +0800

    Add docker

--- packages/backend/src/lib/checkData.ts
+++ packages/backend/src/lib/checkData.ts
@@ -7,7 +7,7 @@ export const checkMail = (mail: string) => {
 };
 
 export const checkPhone = (phone: string) => {
-    const re = /^((13[0-9])|(14[57])|(15[0-3,5-9])|166|(17[035678])|(18[0-9])|(19[89]))\d{8}$/i;
+    const re = /^((13[0-9])|(14[4-9])|(15[0-35-9])|16[67]|(17[013-8])|(18[0-9])|(19[0-9]))\d{8}$/i;
     return re.test(phone);
 };
 
