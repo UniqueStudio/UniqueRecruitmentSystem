88cf6b35c1bcb6f7b5189d30ff9a4a01be367910
winderica <winderica@gmail.com>
2018-08-31 13:31:47 +0800

    Fix bugs

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -33,7 +33,7 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => cb(null, `${req['body'].name} - ${file.originalname}`)
 });
 const upload = multer({ storage: storage, limits: { fileSize: 104857600 } });  // 100MB
-export const redisClient = redis.createClient();
+export const redisClient = redis.createClient(9211);
 export const getAsync = promisify(redisClient.get).bind(redisClient);
 redisClient.on("error", err => {
     console.log("Redis Error: " + err);
