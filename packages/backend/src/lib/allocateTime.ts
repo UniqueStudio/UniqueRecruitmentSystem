bfb0f093afb3c5231f82c94dcf4636e4d3e68e8c
winderica <winderica@gmail.com>
2018-10-20 10:51:05 +0800

    Fix bugs

--- packages/backend/src/lib/allocateTime.ts
+++ packages/backend/src/lib/allocateTime.ts
@@ -14,7 +14,7 @@ const converter = (selections: object[]) => {
 export const allocateTime = (slots: number[], candidates: Candidate[], interview: 1 | 2, dates: string[]) => {
 
     const timeSlots = () => ({
-        morning: ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'],
+        morning: ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30'],
         afternoon: ['13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00'],
         evening: ['18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00']
     });
