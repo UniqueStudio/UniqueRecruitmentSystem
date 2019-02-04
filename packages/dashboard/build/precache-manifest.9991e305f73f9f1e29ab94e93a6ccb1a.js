6dbe96bfbf678a41390f1245346b8653e414b8bf
winderica <winderica@gmail.com>
2019-02-04 15:57:31 +0800

    Fix: fix bug of nginx

---
+++ packages/dashboard/build/precache-manifest.9991e305f73f9f1e29ab94e93a6ccb1a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6eed29bb835be73f45",
+    "url": "/static/js/0.6f6eed29.chunk.js"
+  },
+  {
+    "revision": "0c2e143b5b69445d7130",
+    "url": "/static/js/1.0c2e143b.chunk.js"
+  },
+  {
+    "revision": "01ef40f7e70e711a3901",
+    "url": "/static/js/main.01ef40f7.chunk.js"
+  },
+  {
+    "revision": "6e9f15e8adec3f3f4d44",
+    "url": "/static/js/3.6e9f15e8.chunk.js"
+  },
+  {
+    "revision": "01709cb2b652d1e13675",
+    "url": "/static/js/4.01709cb2.chunk.js"
+  },
+  {
+    "revision": "305fbab27d6876d5107d",
+    "url": "/static/js/5.305fbab2.chunk.js"
+  },
+  {
+    "revision": "72a4bfd188354e5ec146",
+    "url": "/static/js/6.72a4bfd1.chunk.js"
+  },
+  {
+    "revision": "ff3d64f999bec0e960de",
+    "url": "/static/js/7.ff3d64f9.chunk.js"
+  },
+  {
+    "revision": "d4c587f51289d4468aaa",
+    "url": "/static/js/8.d4c587f5.chunk.js"
+  },
+  {
+    "revision": "930f9041a527cbaaf54f",
+    "url": "/static/js/9.930f9041.chunk.js"
+  },
+  {
+    "revision": "f1ac268f4cb29ac724fa",
+    "url": "/static/js/10.f1ac268f.chunk.js"
+  },
+  {
+    "revision": "1344b7d9e919826290a2",
+    "url": "/static/js/11.1344b7d9.chunk.js"
+  },
+  {
+    "revision": "a4b627ba86b25d04b659",
+    "url": "/static/js/12.a4b627ba.chunk.js"
+  },
+  {
+    "revision": "728c3bbc7ffa73e707c0",
+    "url": "/static/js/runtime~main.728c3bbc.js"
+  },
+  {
+    "revision": "f3af399b1c62b7aca9986c432e068efd",
+    "url": "/static/media/logo.f3af399b.webp"
+  },
+  {
+    "revision": "8b84d87bd9d0253441c03c5e64a92b65",
+    "url": "/static/media/404.8b84d87b.webp"
+  },
+  {
+    "revision": "c2b369480e607bb9f4da14184adf8b0e",
+    "url": "/index.html"
+  }
+];
