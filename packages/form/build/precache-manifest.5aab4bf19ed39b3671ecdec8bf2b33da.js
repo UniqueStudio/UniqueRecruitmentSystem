e25359aabe137b72811c5fcf2a0f0ac8c25630be
winderica <winderica@gmail.com>
2019-02-04 09:44:35 +0800

    Fix: fix bugs in nginx config

---
+++ packages/form/build/precache-manifest.5aab4bf19ed39b3671ecdec8bf2b33da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a41f5940a13ae9dd44",
+    "url": "/static/css/main.b27bc43c.chunk.css"
+  },
+  {
+    "revision": "02a41f5940a13ae9dd44",
+    "url": "/static/js/main.02a41f59.chunk.js"
+  },
+  {
+    "revision": "f05f79257a02b6ddf491",
+    "url": "/static/js/1.f05f7925.chunk.js"
+  },
+  {
+    "revision": "1280081bbfb04fd69e7c",
+    "url": "/static/js/2.1280081b.chunk.js"
+  },
+  {
+    "revision": "bab76c268e1a3f807362",
+    "url": "/static/js/runtime~main.bab76c26.js"
+  },
+  {
+    "revision": "fe486369a4f3ca0c0df85ec54229b5cd",
+    "url": "/static/media/header.fe486369.png"
+  },
+  {
+    "revision": "b26629b599c634682d6c7945b7654bd6",
+    "url": "/static/media/arrow.b26629b5.svg"
+  },
+  {
+    "revision": "597780bb1a6e471e018369ee6672ed5b",
+    "url": "/static/media/Hiragino.597780bb.ttf"
+  },
+  {
+    "revision": "76daa78a31320afb7ba89ceb0febeb25",
+    "url": "/index.html"
+  }
+];
--- packages/form/build/service-worker.js
+++ packages/form/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5e31e5548659280f664c50bc23502393.js"
+  "/precache-manifest.5aab4bf19ed39b3671ecdec8bf2b33da.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
