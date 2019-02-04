8bc7bee88483822b6cbcc9e8dbcbb4ee2176d7e3
winderica <winderica@gmail.com>
2019-02-04 15:57:31 +0800

    Fix: fix bug of nginx

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.20f8bd9c4c6bb258d4762af2380f49b9.js"
+  "/precache-manifest.9991e305f73f9f1e29ab94e93a6ccb1a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
