d1a69c5fe4b624f456e0592c6f1da4673234ae87
winderica <winderica@gmail.com>
2019-03-06 20:40:57 +0800

    Update: validate service worker

--- packages/dashboard/src/registerServiceWorker.ts
+++ packages/dashboard/src/registerServiceWorker.ts
@@ -50,6 +50,7 @@ export default function register() {
                 });
             } else {
                 // Is not local host. Just register service worker
+                console.log('Registering...');
                 registerValidSW(swUrl);
             }
         });
