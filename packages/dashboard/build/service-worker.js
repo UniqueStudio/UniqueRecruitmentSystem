377a8162dc9c91c5b94605d89c6e4250d1abd4ed
winderica <winderica@gmail.com>
2019-03-07 18:31:21 +0800

    Update: improve sw

--- packages/dashboard/build/service-worker.js
+++ packages/dashboard/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ae92545198d602b1c4751724285fcf1.js"
+  "/precache-manifest.262d2cbf1ab2a8fe9d2a2d282cc65488.js"
 );
 
 workbox.clientsClaim();
