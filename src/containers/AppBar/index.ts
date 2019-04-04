55d5d1de78ff7dba46995a7f6fd42c78e7dd8420
winderica <winderica@gmail.com>
2019-04-04 21:15:46 +0800

    Update: minor improvements

--- src/containers/AppBar/index.ts
+++ src/containers/AppBar/index.ts
@@ -3,8 +3,6 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { Dispatch } from 'redux';
 
 import {
-    GetCandidatesStart,
-    getCandidatesStart,
     Logout,
     logout,
     SetGroup,
@@ -30,18 +28,13 @@ const mapStateToProps =
         ...ownProps,
     });
 
-type DispatchType = Dispatch<ToggleDrawer | Logout | GetCandidatesStart | SetGroup | SetSteps>;
+type DispatchType = Dispatch<ToggleDrawer | Logout | SetGroup | SetSteps>;
 
 const mapDispatchToProps = (dispatch: DispatchType) => ({
     toggleDrawer: () => dispatch(toggleDrawer()),
     logout: () => dispatch(logout()),
     setGroup: (group: Group) => dispatch(setGroup(group)),
     setSteps: (stepType: number) => dispatch(setSteps(stepType)),
-    getCandidates: ({ group, step, title }: {
-        group?: Group,
-        step?: number,
-        title: string
-    }) => dispatch(getCandidatesStart(title, group, step)),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AppBar));
