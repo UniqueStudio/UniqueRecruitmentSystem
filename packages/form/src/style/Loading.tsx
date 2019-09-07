a2c1fd84c2e3b39d0fa1bd9e3b7dc66ed1c0703a
wangnengjie <751614701@qq.com>
2019-09-07 12:01:16 +0800

    refactor(component loading): use hook to refactor Loading

--- packages/form/src/style/Loading.tsx
+++ packages/form/src/style/Loading.tsx
@@ -1,20 +1,24 @@
-import { createStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+interface Props {
+    open: boolean;
+}
 
 const styles = createStyles({
-    root: {
+    root: ({ open }: Props) => ({
         position: 'fixed',
         width: '100vw',
         height: '100vh',
         top: 0,
         left: 0,
         backgroundColor: 'rgba(0,0,0,0.5)',
-        display: 'flex',
+        display: open ? 'flex' : 'none',
         justifyContent: 'center',
         alignItems: 'center'
-    },
+    }),
     loading: {
         color: 'rgb(31,84,122)'
     }
 });
 
-export default styles;
+export default makeStyles(styles);
