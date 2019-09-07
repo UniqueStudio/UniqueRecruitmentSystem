5ef91436080e8fb0bf89d9f7296746a6b6981466
wangnengjie <751614701@qq.com>
2019-09-07 15:44:40 +0800

    refactor(component uploadprogress): use hook to refactor uploadprogress

--- packages/form/src/component/UploadProgress/index.tsx
+++ packages/form/src/component/UploadProgress/index.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
+import React, { FC, memo } from 'react';
 
 import { LinearProgress } from '@material-ui/core';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import styles from '../../style/UploadProgress';
+import useStyles from '../../style/UploadProgress';
 
-interface Props extends WithStyles<typeof styles> {
+interface UploadProgressProps {
     progress: string | number;
 }
 
-function UploadProgress(props: Props) {
-    const { container, barColorPrimary, colorPrimary, root } = props.classes;
-    let progress = props.progress;
-    typeof progress === 'string' && (progress = Number.parseInt(progress, 10));
-
+const UploadProgress: FC<UploadProgressProps> = memo(({ progress }) => {
+    const { container, barColorPrimary, colorPrimary, root } = useStyles();
     return (
         <div className={container}>
             <LinearProgress
-                value={progress}
+                value={typeof progress === 'string' ? Number.parseInt(progress, 10) : progress}
                 variant='determinate'
                 color='primary'
                 classes={{ root, barColorPrimary, colorPrimary }}
             />
         </div>
     );
-}
+});
 
-export default withStyles(styles)(UploadProgress);
+export default UploadProgress;
