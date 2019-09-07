79243b9d239b76782e2a23e72333eeb899c76db1
wangnengjie <751614701@qq.com>
2019-09-07 15:26:56 +0800

    refactor(component textarea): use hook to refactor textarea

--- packages/form/src/component/TextArea/index.tsx
+++ packages/form/src/component/TextArea/index.tsx
@@ -1,27 +1,22 @@
-import React, { PureComponent } from 'react';
+import React, { FC, memo } from 'react';
 
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import classNames from 'classnames';
-import styles from '../../style/TextArea';
-// import Button from '../Button';
+import useStyles from '../../style/TextArea';
 
-interface Props extends WithStyles<typeof styles> {
+interface TextAreaProps {
     onChange: (e: React.ChangeEvent) => void;
 }
 
-class TextArea extends PureComponent<Props> {
-    render() {
-        const { classes, onChange } = this.props;
-        return (
-            <div className={classNames(classes.container, classes.border, classes.font)}>
-                {/* <Button name='自我介绍' bgColor='primary' textColor='white' className='textAreaLabel disabled' /> */}
-                <div className={classes.textLabel}>
-                    <div>自我介绍</div>
-                </div>
-                <textarea className={classes.textarea} onChange={onChange} />
+const TextArea: FC<TextAreaProps> = memo(({ onChange }) => {
+    const classes = useStyles();
+    return (
+        <div className={classNames(classes.container, classes.border, classes.font)}>
+            <div className={classes.textLabel}>
+                <div>自我介绍</div>
             </div>
-        );
-    }
-}
+            <textarea className={classes.textarea} onChange={onChange} />
+        </div>
+    );
+});
 
-export default withStyles(styles)(TextArea);
+export default TextArea;
