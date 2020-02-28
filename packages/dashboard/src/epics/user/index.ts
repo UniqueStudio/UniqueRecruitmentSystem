6c2f116f2ffec4701cd25a72078de545edb9b360
Colin <qingyu.wang@aliyun.com>
2020-02-28 22:31:07 +0800

    feat(group): add checkbox in group member and add epics for submit

--- packages/dashboard/src/epics/user/index.ts
+++ packages/dashboard/src/epics/user/index.ts
@@ -1,6 +1,7 @@
 import { getGroupEpic } from './getGroup';
 import { getInfoEpic } from './getInfo';
 import { getQRCodeEpic, loginEpic, scanQRCodeEpic } from './login';
+import { setGroupAdminEpic} from './setAdmin';
 import { setInfoEpic } from './setInfo';
 
-export default [getQRCodeEpic, scanQRCodeEpic, loginEpic, getInfoEpic, getGroupEpic, setInfoEpic];
+export default [getQRCodeEpic, scanQRCodeEpic, loginEpic, getInfoEpic, getGroupEpic, setInfoEpic, setGroupAdminEpic];
