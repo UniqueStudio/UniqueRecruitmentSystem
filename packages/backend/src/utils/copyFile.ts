00b12c973a0e9813394a4b2db0effe81cbee4758
Colin <colinwang.0616@gmail.com>
2020-09-14 13:21:28 +0800

    fix: remove promisify due to mkdirp api change

--- packages/backend/src/utils/copyFile.ts
+++ packages/backend/src/utils/copyFile.ts
@@ -4,10 +4,10 @@ import { join } from 'path';
 import { promisify } from 'util';
 
 const copy = promisify(fs.copyFile);
-const mkdir = promisify(mkdirp);
+// const mkdir = promisify(mkdirp);
 
 export const copyFile = async (oldPath: string, newDirectory: string, name: string) => {
-    await mkdir(newDirectory);
+    await mkdirp(newDirectory);
     const newPath = join(newDirectory, name);
     await copy(oldPath, newPath);
     return newPath;
