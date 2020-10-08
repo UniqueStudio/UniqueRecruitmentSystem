d256e43d1f206cc293a250bccd6e73bd984b23de
Rabbit <ttzztztz@outlook.com>
2020-10-08 22:16:00 +0800

    feat: evening allocate time slot (#22)
    Should avoid allocating 18:00 for candidate to interview

--- packages/backend/src/utils/allocateTime.ts
+++ packages/backend/src/utils/allocateTime.ts
@@ -13,7 +13,7 @@ export const allocateTime = (interviewTime: Time[], candidates: Candidate[], typ
     const timeSlots = {
         morning: ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '08:00', '08:30'],
         afternoon: ['14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '13:30', '14:00'],
-        evening: ['18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30']
+        evening: ['18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '18:00']
     };
 
     let selections = candidates.map(({ interviews: { [type]: { selection } }, _id }) => ({ selection, id: _id }));
