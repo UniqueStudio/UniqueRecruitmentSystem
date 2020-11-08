20d0428efaf01b59f6734b0e8984b9b056df3148
winderica <winderica@gmail.com>
2020-11-08 18:46:14 +0800

    Update: return the name of the most `satisfying` recruitment

--- packages/backend/src/actions/recruitment/getPendingTitles.ts
+++ packages/backend/src/actions/recruitment/getPendingTitles.ts
@@ -7,8 +7,11 @@ export const getPendingTitles: Handler = async (req, res, next) => {
         const pending = await RecruitmentRepo.query({ stop: { $gt: Date.now() }, begin: { $lt: Date.now() } });
         if (pending.length === 0) {
             return next(errorRes('No pending recruitment!', 'info'));
+        } else if (pending.length === 1) { // return an array of one element for backward compatibility
+            res.json({ data: [pending[0].title], type: 'success' });
+        } else {
+            res.json({ data: [pending.map((item) => item.title).find((title) => !title.includes('O'))], type: 'success' });
         }
-        res.json({ data: pending.map((item) => item.title), type: 'success' });
     } catch (error) {
         return next(error);
     }
