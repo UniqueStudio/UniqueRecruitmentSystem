d62abcea1b20f8c43532ba038adf8e96d62ef3d6
wangnengjie <751614701@qq.com>
2021-02-17 22:09:25 +0800

    feat: add login logic

--- packages/candidate-dashboard/src/pages/login.tsx
+++ packages/candidate-dashboard/src/pages/login.tsx
@@ -10,13 +10,18 @@ import {
   Avatar,
   useTheme,
   useMediaQuery,
+  CircularProgress,
 } from '@material-ui/core';
 import { blue } from '@material-ui/core/colors';
 import { DashboardRounded as DashboardIcon } from '@material-ui/icons';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
-// import { useAppDispatch } from 'store';
-// import { showSnackbar } from 'store/component';
+import { getVerificationCode, GetVerificationCodeResp, loginCandidate, LoginCandidateResp } from 'services';
+import { useAppDispatch } from 'store';
+import { showSnackbar } from 'store/component';
+import { setToken } from 'utils/token';
+import { checkPhone } from 'utils/validators';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -34,22 +39,33 @@ const useStyles = makeStyles((theme) => ({
   iconBgColor: {
     backgroundColor: blue[50],
   },
+  buttonProgress: {
+    color: theme.palette.common.white,
+  },
 }));
 
 const Login: NextPage = () => {
+  const router = useRouter();
   const classes = useStyles();
   const theme = useTheme();
-  // const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
   const matchMedia = useMediaQuery(theme.breakpoints.down('xs'));
 
   const [phone, setPhone] = useState('');
   const [code, setCode] = useState('');
+  const [login, setLogin] = useState(false);
   const [countdown, setCountdown] = useState({ time: 0, send: false, id: undefined as number | undefined });
 
-  // Todo: add request logic
-  const handleSendCode = async () => {
-    // dispatch(showSnackbar({ type: 'info', message: 'this is a test', _key: Date.now() }));
+  const handleSendCode = () => {
     if (countdown.send) {
+      return;
+    }
+    if (!phone) {
+      dispatch(showSnackbar({ type: 'warning', message: '请填写手机号码！' }));
+      return;
+    }
+    if (!checkPhone(phone)) {
+      dispatch(showSnackbar({ type: 'warning', message: '手机号码格式不正确!' }));
       return;
     }
     const id = window.setInterval(() => {
@@ -65,6 +81,43 @@ const Login: NextPage = () => {
       });
     }, 1000);
     setCountdown({ time: 59, send: true, id });
+    dispatch(async (dispatch) => {
+      const res: GetVerificationCodeResp = await getVerificationCode(phone).catch((e) => {
+        return { type: 'error', message: e?.message };
+      });
+      dispatch(showSnackbar({ type: res.type, message: res.type == 'success' ? '验证码已发送' : res?.message }));
+    });
+  };
+
+  const handleLogin = () => {
+    if (login) {
+      return;
+    }
+    if (!phone) {
+      dispatch(showSnackbar({ type: 'warning', message: '请填写手机号码！' }));
+      return;
+    }
+    if (!checkPhone(phone)) {
+      dispatch(showSnackbar({ type: 'warning', message: '手机号码格式不正确!' }));
+      return;
+    }
+    if (!code) {
+      dispatch(showSnackbar({ type: 'warning', message: '请填写验证码！' }));
+      return;
+    }
+    setLogin(true);
+    dispatch(async (dispatch) => {
+      const res: LoginCandidateResp = await loginCandidate(phone, code).catch((e) => {
+        return { type: 'error', message: e?.message };
+      });
+      setLogin(false);
+      if (res.type != 'success') {
+        dispatch(showSnackbar({ type: res.type, message: res?.message }));
+      } else {
+        setToken(res.token ?? '');
+        router.push('/');
+      }
+    });
   };
 
   return (
@@ -122,8 +175,8 @@ const Login: NextPage = () => {
           </Grid>
 
           <Grid item xs={4}>
-            <Button variant='contained' color='primary' fullWidth>
-              登陆
+            <Button variant='contained' color='primary' fullWidth onClick={handleLogin}>
+              {login ? <CircularProgress size={24} classes={{ colorPrimary: classes.buttonProgress }} /> : '登陆'}
             </Button>
           </Grid>
 
--- packages/candidate-dashboard/src/services/sms.ts
+++ packages/candidate-dashboard/src/services/sms.ts
@@ -1,6 +1,5 @@
 import { HOST } from 'config/consts';
 import { MessageType } from 'config/types';
-import { checkPhone } from 'utils/validators';
 
 const prefix = 'sms';
 
@@ -10,12 +9,6 @@ export interface GetVerificationCodeResp {
 }
 
 export const getVerificationCode: (phone: string) => Promise<GetVerificationCodeResp> = async (phone: string) => {
-  if (!phone) {
-    return { type: 'warning', message: '请填写手机号码！' };
-  }
-  if (!checkPhone(phone)) {
-    return { type: 'warning', message: '手机号码格式不正确!' };
-  }
   const resp = await fetch(`${HOST}/${prefix}/verification/candidate/${phone}`);
   const result: GetVerificationCodeResp = await resp.json();
   return result;
