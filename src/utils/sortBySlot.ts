723941b8f0c7ebfdceb7c0fcbd7c61ec32aa0d36
Colin <colinwang.0616@gmail.com>
2021-03-20 20:33:20 +0800

    feat(table): add add mannual sort table columns

--- src/utils/sortBySlot.ts
+++ src/utils/sortBySlot.ts
@@ -4,9 +4,13 @@ export const teamSort = (i: Candidate, j: Candidate) => {
     return sortBySlot(i.interviews.team.allocation, j.interviews.team.allocation);
 };
 
+export const teamSortDesc = (i: Candidate, j: Candidate) => -teamSort(i, j);
+
 export const groupSort = (i: Candidate, j: Candidate) => {
     return sortBySlot(i.interviews.group.allocation, j.interviews.group.allocation);
 };
+
+export const groupSortDesc = (i: Candidate, j: Candidate) => -groupSort(i, j);
 
 export const sortBySlot = (iAllocation: number, jAllocation: number) => {
     if (!iAllocation && jAllocation) return 1;
