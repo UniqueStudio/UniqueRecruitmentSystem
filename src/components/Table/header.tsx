2ba31c8cd709665c5926a27a1e89d60b00157b98
Colin Wang <colinwang.0616@gmail.com>
2021-03-21 17:32:56 +0800

    Fix/table header (#37)
    * fix(table): fix table header order and make state unsortable
    
    * chore(build): remove all builds

--- src/components/Table/header.tsx
+++ src/components/Table/header.tsx
@@ -8,8 +8,8 @@ import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 import { Order } from '../../utils/order';
 
-const sortableHeads = ['姓名', '组别', '分配结果', '选择情况'] as const;
-const otherHeads = ['手动调整'] as const;
+const sortableHeads = ['姓名', '组别', '分配结果'] as const;
+const otherHeads = ['选择情况', '手动调整'] as const;
 
 export type OrderBy = typeof sortableHeads[number];
 
@@ -39,6 +39,7 @@ export const EnhancedTableHead: React.FC<EnhancedTableProps> = ({
             <TableRow>
                 <TableCell padding='checkbox'>
                     <Checkbox
+                        disabled={rowCount === 0}
                         indeterminate={numSelected > 0 && numSelected < rowCount}
                         checked={rowCount > 0 && numSelected === rowCount}
                         onChange={onCheckAll}
