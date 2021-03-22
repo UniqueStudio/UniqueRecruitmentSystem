7cf16ed4a105fe786a2de6a5f8b7b4e913aec5c3
winderica <winderica@gmail.com>
2021-03-22 16:39:11 +0800

    Fix: fix recursive dependency
    Maybe snowpack is not good at handling recursive dependency,
    so now stores/user.ts no longer depends on apis/rest.ts.

--- packages/dashboard/src/apis/rest.ts
+++ packages/dashboard/src/apis/rest.ts
@@ -5,6 +5,8 @@ import { Candidate, Group, Recruitment, Step, Time, User } from '@config/types';
 import { stores } from '@stores/index';
 import { localStorage } from '@utils/storage';
 
+const { $candidate, $component, $user, $recruitment } = stores;
+
 class Endpoint {
     static base = API;
 
@@ -51,7 +53,7 @@ const client = axios.create({
     baseURL: Endpoint.base,
 });
 
-export const setAuthToken = (token: string) => {
+const setAuthToken = (token: string) => {
     (client.defaults.headers as { common: Record<string, string> }).common['Authorization'] = `Bearer ${token}`;
 };
 
@@ -60,22 +62,23 @@ const apiWrapper = async <T>(
     onSuccess: (data: SuccessResponse<T>) => void | Promise<void>,
     onFailure?: () => void | Promise<void>,
 ) => {
-    stores.$component.setProgress(true);
+    $component.setProgress(true);
+    setAuthToken($user.token);
     try {
         const { data } = await action();
-        stores.$component.setProgress(false);
+        $component.setProgress(false);
         if (data.type === 'success') {
             await onSuccess(data);
         } else {
             const messages = data.messages || [data.message!];
             messages.forEach((message) => {
-                stores.$component.enqueueSnackbar(message, data.type || 'error');
+                $component.enqueueSnackbar(message, data.type || 'error');
             });
             await onFailure?.();
         }
     } catch ({ message }) {
-        stores.$component.setProgress(false);
-        stores.$component.enqueueSnackbar(message, 'error');
+        $component.setProgress(false);
+        $component.enqueueSnackbar(message, 'error');
         await onFailure?.();
     }
 };
@@ -84,15 +87,15 @@ export const allocateAll = (interviewType: 'group' | 'team') =>
     apiWrapper(
         () =>
             client.put<R<{ allocations: { id: string; time: number }[] }>>(Endpoint.interview(interviewType), {
-                title: stores.$recruitment.viewing,
+                title: $recruitment.viewing,
             }),
         ({ allocations }) => {
-            stores.$candidate.allocateAll(allocations, interviewType);
+            $candidate.allocateAll(allocations, interviewType);
             const failed = allocations.filter(({ time }) => !time).length;
             if (failed) {
-                stores.$component.enqueueSnackbar(`有${failed}位候选人没有分配到时间！(不包括未选择时间的)`, 'info');
+                $component.enqueueSnackbar(`有${failed}位候选人没有分配到时间！(不包括未选择时间的)`, 'info');
             } else {
-                stores.$component.enqueueSnackbar('所有候选人均分配了时间！(不包括未选择时间的)', 'success');
+                $component.enqueueSnackbar('所有候选人均分配了时间！(不包括未选择时间的)', 'success');
             }
         },
     );
@@ -101,8 +104,8 @@ export const allocateOne = (interviewType: 'group' | 'team', cid: string, time: 
     apiWrapper(
         () => client.put<R>(Endpoint.interview(interviewType, cid), { time }),
         () => {
-            stores.$candidate.allocateOne(interviewType, cid, time);
-            stores.$component.enqueueSnackbar('设置成功！', 'success');
+            $candidate.allocateOne(interviewType, cid, time);
+            $component.enqueueSnackbar('设置成功！', 'success');
         },
     );
 
@@ -115,27 +118,28 @@ export const getCandidates = (title: string, group?: Group, step?: Step) =>
                 let data = candidates;
                 if (step) data = data.filter((candidate) => candidate.step === step);
                 if (group) data = data.filter((candidate) => candidate.group === group);
-                stores.$candidate.addCandidates(data);
-                stores.$component.toggleFabOff();
-                stores.$component.enqueueSnackbar('成功获取候选人信息（缓存）', 'success');
+                $candidate.addCandidates(data);
+                $component.toggleFabOff();
+                $component.enqueueSnackbar('成功获取候选人信息（缓存）', 'success');
             }
             return client.get<R<{ data: Candidate[] }>>(Endpoint.candidates(title, group, step));
         },
         ({ data }) => {
-            stores.$candidate.addCandidates(data);
-            stores.$recruitment.setViewingRecruitment(title);
-            stores.$component.toggleFabOff();
-            stores.$component.enqueueSnackbar('成功获取候选人信息（线上）', 'success');
+            $candidate.addCandidates(data);
+            $recruitment.setViewingRecruitment(title);
+            $component.toggleFabOff();
+            $component.enqueueSnackbar('成功获取候选人信息（线上）', 'success');
         },
     );
 
 export const getResume = async (cid: string) => {
-    stores.$component.setProgress(true);
+    $component.setProgress(true);
+    setAuthToken($user.token);
     try {
         const { data, headers } = await client.get<Blob>(Endpoint.resume(cid), {
             responseType: 'blob',
             onDownloadProgress(event: ProgressEvent) {
-                stores.$component.setResumeProgress(event.loaded / event.total, cid);
+                $component.setResumeProgress(event.loaded / event.total, cid);
             },
         });
         let filename = 'resume';
@@ -159,20 +163,20 @@ export const getResume = async (cid: string) => {
         a.click();
         document.body.removeChild(a);
         URL.revokeObjectURL(url);
-        stores.$component.setResumeProgress(0, cid);
+        $component.setResumeProgress(0, cid);
     } catch ({ message }) {
-        stores.$component.enqueueSnackbar(message, 'error');
+        $component.enqueueSnackbar(message, 'error');
     }
-    stores.$component.setProgress(false);
+    $component.setProgress(false);
 };
 
 export const getRecruitments = () => {
     const recruitments = localStorage.getItem('recruitments');
     const viewing = localStorage.getItem('viewing');
-    const shouldUpdateRecruitment = stores.$recruitment.shouldUpdateRecruitment;
+    const shouldUpdateRecruitment = $recruitment.shouldUpdateRecruitment;
     if (recruitments && !shouldUpdateRecruitment) {
-        stores.$recruitment.setRecruitments(recruitments);
-        stores.$component.enqueueSnackbar('成功获取招新信息', 'success');
+        $recruitment.setRecruitments(recruitments);
+        $component.enqueueSnackbar('成功获取招新信息', 'success');
         return;
     }
     return apiWrapper(
@@ -180,9 +184,9 @@ export const getRecruitments = () => {
         ({ data }) => {
             data.sort((prev, next) => prev.begin - next.begin);
             const newViewing = viewing ? viewing : data.slice(-1)[0] ? data.slice(-1)[0].title : '';
-            stores.$recruitment.setRecruitments(data);
-            stores.$recruitment.setViewingRecruitment(newViewing);
-            stores.$component.enqueueSnackbar('成功获取招新信息', 'success');
+            $recruitment.setRecruitments(data);
+            $recruitment.setViewingRecruitment(newViewing);
+            $component.enqueueSnackbar('成功获取招新信息', 'success');
         },
     );
 };
@@ -191,8 +195,8 @@ export const launchRecruitment = (info: Partial<Recruitment & { code: string }>)
     apiWrapper(
         () => client.post<R>(Endpoint.recruitments, info),
         () => {
-            stores.$recruitment.setShouldUpdateRecruitment();
-            stores.$component.enqueueSnackbar('已成功发起招新！', 'success');
+            $recruitment.setShouldUpdateRecruitment();
+            $component.enqueueSnackbar('已成功发起招新！', 'success');
             return getRecruitments();
         },
     );
@@ -209,8 +213,8 @@ export const setRecruitment = (data: {
     apiWrapper(
         () => client.put<R>(Endpoint.recruitment(data.title), data),
         () => {
-            stores.$recruitment.setShouldUpdateRecruitment();
-            stores.$component.enqueueSnackbar('已成功修改招新信息！', 'success');
+            $recruitment.setShouldUpdateRecruitment();
+            $component.enqueueSnackbar('已成功修改招新信息！', 'success');
             return getRecruitments();
         },
     );
@@ -218,39 +222,39 @@ export const setRecruitment = (data: {
 export const getVerifyCode = () =>
     apiWrapper(
         () => client.get<R>(Endpoint.verification),
-        () => stores.$component.enqueueSnackbar('验证码已发送！', 'success'),
+        () => $component.enqueueSnackbar('验证码已发送！', 'success'),
     );
 
 export const sendSMS = (content: Record<string, unknown>) =>
     apiWrapper(
         () => client.post<R>(Endpoint.sms, content),
-        () => stores.$component.enqueueSnackbar('已成功发送短信！', 'success'),
+        () => $component.enqueueSnackbar('已成功发送短信！', 'success'),
     );
 
 export const getGroup = () => {
     const groupInfo = localStorage.getItem('group');
-    if (groupInfo && !stores.$user.firstLoad) {
-        stores.$user.setGroupInfo(groupInfo);
+    if (groupInfo && !$user.firstLoad) {
+        $user.setGroupInfo(groupInfo);
         return;
     }
     return apiWrapper(
         () => client.get<R<{ data: User[] }>>(Endpoint.group),
-        ({ data }) => stores.$user.setGroupInfo(data),
+        ({ data }) => $user.setGroupInfo(data),
     );
 };
 
 export const getUserInfo = () => {
     const user = localStorage.getItem('user');
     if (user) {
-        stores.$user.setUserInfo(user);
-        stores.$candidate.setGroup(user.group);
+        $user.setUserInfo(user);
+        $candidate.setGroup(user.group);
         return;
     }
     return apiWrapper(
         () => client.get<R<{ data: User }>>(Endpoint.user),
         ({ data }) => {
-            stores.$user.setUserInfo(data);
-            stores.$candidate.setGroup(data.group);
+            $user.setUserInfo(data);
+            $candidate.setGroup(data.group);
         },
     );
 };
@@ -261,13 +265,13 @@ export const loginViaQRCode = () =>
         ({ key }) =>
             apiWrapper(
                 () => {
-                    stores.$user.setQRCode(key);
-                    stores.$component.enqueueSnackbar('请尽快用企业微信扫描二维码！', 'success');
+                    $user.setQRCode(key);
+                    $component.enqueueSnackbar('请尽快用企业微信扫描二维码！', 'success');
                     return client.get<R<{ token: string }>>(Endpoint.qrCode(key));
                 },
                 ({ token }) => {
-                    stores.$user.setToken(token);
-                    stores.$component.enqueueSnackbar('已成功登录！', 'success');
+                    $user.setToken(token);
+                    $component.enqueueSnackbar('已成功登录！', 'success');
                 },
             ),
     );
@@ -276,8 +280,8 @@ export const loginViaPassword = (phone: string, password: string) =>
     apiWrapper(
         () => client.post<R<{ token: string }>>(Endpoint.login, { phone, password }),
         ({ token }) => {
-            stores.$user.setToken(token);
-            stores.$component.enqueueSnackbar('已成功登录！', 'success');
+            $user.setToken(token);
+            $component.enqueueSnackbar('已成功登录！', 'success');
         },
     );
 
@@ -285,8 +289,8 @@ export const setGroupAdmin = (data: { group: string; who: string[] }) =>
     apiWrapper(
         () => client.put<R<{ newAdmins: string[] }>>(Endpoint.admin, data),
         ({ newAdmins }) => {
-            stores.$user.setGroupAdmins(newAdmins);
-            stores.$component.enqueueSnackbar('已成功修改管理员！', 'success');
+            $user.setGroupAdmins(newAdmins);
+            $component.enqueueSnackbar('已成功修改管理员！', 'success');
         },
     );
 
@@ -294,7 +298,7 @@ export const setUserInfo = (data: { phone: string; mail: string; password?: stri
     apiWrapper(
         () => client.put<R<{ newAdmins: string[] }>>(Endpoint.user, data),
         () => {
-            stores.$user.setUserInfo(data);
-            stores.$component.enqueueSnackbar('已成功修改信息！', 'success');
+            $user.setUserInfo(data);
+            $component.enqueueSnackbar('已成功修改信息！', 'success');
         },
     );
