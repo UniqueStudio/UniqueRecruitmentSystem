ef5b696e8d7ed3f0c7f054cd755e22c8afdae51a
winderica <winderica@gmail.com>
2021-03-22 18:16:09 +0800

    Update: extract table body and align table header with body

--- src/components/Table/header.tsx
+++ src/components/Table/header.tsx
@@ -1,6 +1,7 @@
 import { Checkbox, TableCell, TableHead, TableRow, TableSortLabel } from '@material-ui/core';
-import React from 'react';
+import React, { ChangeEvent, FC, MouseEvent } from 'react';
 
+import useStyles from '@styles/data';
 import { Order } from '@utils/order';
 
 const sortableHeads = ['姓名', '组别', '分配结果'] as const;
@@ -8,41 +9,38 @@ const otherHeads = ['选择情况', '手动调整'] as const;
 
 export type OrderBy = typeof sortableHeads[number];
 
-interface EnhancedTableProps {
+interface Props {
     numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: OrderBy) => void;
-    onCheckAll: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onRequestSort: (event: MouseEvent<unknown>, property: OrderBy) => void;
+    onCheckAll: (event: ChangeEvent<HTMLInputElement>) => void;
     order: Order;
     orderBy: OrderBy;
     rowCount: number;
 }
 
-export const EnhancedTableHead: React.FC<EnhancedTableProps> = ({
-    onCheckAll,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-}) => {
-    const createSortHandler = (property: OrderBy) => (event: React.MouseEvent<unknown>) => {
+export const Header: FC<Props> = ({ onCheckAll, order, orderBy, numSelected, rowCount, onRequestSort }) => {
+    const classes = useStyles();
+
+    const createSortHandler = (property: OrderBy) => (event: React.MouseEvent<HTMLSpanElement>) => {
         onRequestSort(event, property);
     };
 
     return (
         <TableHead>
             <TableRow>
-                <TableCell padding='checkbox'>
+                <TableCell classes={{ root: classes.tableCell }} padding='checkbox'>
                     <Checkbox
                         disabled={rowCount === 0}
                         indeterminate={numSelected > 0 && numSelected < rowCount}
                         checked={rowCount > 0 && numSelected === rowCount}
                         onChange={onCheckAll}
-                        inputProps={{ 'aria-label': 'select all desserts' }}
                     />
                 </TableCell>
                 {sortableHeads.map((headCell) => (
-                    <TableCell key={headCell} sortDirection={orderBy === headCell ? order : false}>
+                    <TableCell
+                        key={headCell}
+                        classes={{ root: classes.tableCell }}
+                        sortDirection={orderBy === headCell ? order : false}>
                         <TableSortLabel
                             active={orderBy === headCell}
                             direction={orderBy === headCell ? order : 'asc'}
@@ -52,7 +50,9 @@ export const EnhancedTableHead: React.FC<EnhancedTableProps> = ({
                     </TableCell>
                 ))}
                 {otherHeads.map((headCell) => (
-                    <TableCell key={headCell}>{headCell}</TableCell>
+                    <TableCell key={headCell} classes={{ root: classes.tableCell }}>
+                        {headCell}
+                    </TableCell>
                 ))}
             </TableRow>
         </TableHead>
