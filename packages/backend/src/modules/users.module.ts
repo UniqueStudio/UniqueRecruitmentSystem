e51fee3194aa704046f8fdf588a6cb6589e975ad
winderica <winderica@gmail.com>
2021-03-24 00:28:17 +0800

    Fix: add AppConfigService to providers of UsersModule

--- packages/backend/src/modules/users.module.ts
+++ packages/backend/src/modules/users.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from '@controllers/users.controller';
 import { UserEntity } from '@entities/user.entity';
 import { AuthModule } from '@modules/auth.module';
+import { AppConfigService } from '@services/config.service';
 import { UsersService } from '@services/users.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserEntity]), forwardRef(() => AuthModule)],
-    providers: [UsersController, UsersService],
+    providers: [UsersController, UsersService, AppConfigService],
     exports: [UsersService],
 })
 export class UsersModule {}
