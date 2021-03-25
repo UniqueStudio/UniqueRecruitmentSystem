73ead190bbd85bbf28173eaf50da4ad56e5e5fd7
winderica <winderica@gmail.com>
2021-03-25 15:21:37 +0800

    Update: make interface more generic

--- packages/backend/src/interfaces/request.interface.ts
+++ packages/backend/src/interfaces/request.interface.ts
@@ -2,6 +2,6 @@ import { Request } from 'express';
 
 import { UserEntity } from '@entities/user.entity';
 
-export interface RequestWithUser extends Request {
+export interface RequestWithUser<B = unknown, P = { [key: string]: string }> extends Request<P, unknown, B> {
     user?: UserEntity;
 }
