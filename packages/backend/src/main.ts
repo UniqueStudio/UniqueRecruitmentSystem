865196d6f960cb8baaf411f659d3a9e650433a65
winderica <winderica@gmail.com>
2021-03-25 12:29:51 +0800

    Update: swagger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from '@modules/app.module';
 
+const config = new DocumentBuilder().setVersion('3.0').build();
+
 void (async () => {
     const app = await NestFactory.create(AppModule, { cors: true });
+    SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
     await app.listen(app.get(ConfigService).get('PORT')!);
 })();
