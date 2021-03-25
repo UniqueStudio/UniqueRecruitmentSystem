5f642ad23c29a128dd976d9b9abce353dbbfb78c
winderica <winderica@gmail.com>
2021-03-25 16:54:52 +0800

    Fix: now @User decorator works

--- packages/backend/src/decorators/user.decorator.ts
+++ packages/backend/src/decorators/user.decorator.ts
@@ -1,5 +1,7 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 import { RequestWithUser } from '@interfaces/request.interface';
 
-export const User = createParamDecorator((_, { user }: RequestWithUser) => user);
+export const User = createParamDecorator(
+    (_, context: ExecutionContext) => context.switchToHttp().getRequest<RequestWithUser>().user,
+);
