e123358ca3db7534e78fc2eee557cb1972e215a5
winderica <winderica@gmail.com>
2021-03-26 00:16:32 +0800

    Fix: handle #41 in a better way

--- packages/dashboard/src/utils/titleConverter.ts
+++ packages/dashboard/src/utils/titleConverter.ts
@@ -1,18 +1,16 @@
+const map: Record<string, string> = {
+    S: '春季招新',
+    C: '夏令营招新',
+    A: '秋季招新',
+    春季招新: 'S',
+    夏令营招新: 'C',
+    秋季招新: 'A',
+    春招: 'S',
+    夏令营: 'C',
+    秋招: 'A',
+};
+
 export const titleConverter = (title: string) => {
-    try {
-        const last = title.slice(4);
-        const abbr = ['S', 'C', 'A'];
-        const full = ['春招', '夏令营', '秋招'];
-        const i = abbr.indexOf(last);
-        if (i >= 0) {
-            return title.slice(0, 4) + full[i];
-        }
-        const j = full.indexOf(last);
-        if (j >= 0) {
-            return title.slice(0, 4) + abbr[j];
-        }
-        return '';
-    } catch (e) {
-        return '';
-    }
+    const suffix = map[title.slice(4)];
+    return suffix ? title.slice(0, 4) + suffix : '';
 };
