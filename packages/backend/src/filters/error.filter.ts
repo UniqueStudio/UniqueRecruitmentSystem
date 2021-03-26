1d52fed03137f8d0fa99837dc8d0db307221d14b
winderica <winderica@gmail.com>
2021-03-27 01:05:37 +0800

    Update: catch typeorm errors

--- packages/backend/src/filters/error.filter.ts
+++ packages/backend/src/filters/error.filter.ts
@@ -1,12 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
+import { QueryFailedError, EntityNotFoundError } from 'typeorm';
 
 import { Status } from '@constants/enums';
 import { FailureResponse } from '@interfaces/response.interface';
 
 @Catch()
-export class ErrorFilter implements ExceptionFilter {
-    catch(exception: { message?: string }, host: ArgumentsHost) {
+export class ErrorFilter<T extends Error = Error> implements ExceptionFilter<T> {
+    catch(exception: T, host: ArgumentsHost) {
         const res = host.switchToHttp().getResponse<Response<FailureResponse>>();
 
         if (exception instanceof HttpException) {
@@ -15,6 +16,16 @@ export class ErrorFilter implements ExceptionFilter {
             res.status(status).json({
                 status: status < HttpStatus.INTERNAL_SERVER_ERROR ? Status.warning : Status.error,
                 message: Array.isArray(message) ? message.join(', ') : message.toString(),
+            });
+        } else if (exception instanceof QueryFailedError) {
+            res.status(HttpStatus.BAD_REQUEST).json({
+                status: Status.error,
+                message: exception.message,
+            });
+        } else if (exception instanceof EntityNotFoundError) {
+            res.status(HttpStatus.NOT_FOUND).json({
+                status: Status.error,
+                message: exception.message,
             });
         } else {
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
