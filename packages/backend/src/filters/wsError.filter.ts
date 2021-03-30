6fd59835a7fdcd24aae6572ddaf9a3cd281cdf39
winderica <winderica@gmail.com>
2021-03-30 16:56:56 +0800

    Update: add error filter for websocket

---
+++ packages/backend/src/filters/wsError.filter.ts
@@ -0,0 +1,30 @@
+import { ArgumentsHost, Catch } from '@nestjs/common';
+import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { EntityNotFoundError, QueryFailedError } from 'typeorm';
+
+import { Status } from '@constants/enums';
+
+@Catch()
+export class WsErrorFilter<T extends Error = Error> extends BaseWsExceptionFilter<T> {
+    catch(exception: T, host: ArgumentsHost) {
+        const res = host.switchToWs().getClient<Socket>();
+
+        if (exception instanceof WsException) {
+            res.emit('exception', {
+                status: Status.warning,
+                message: exception.message,
+            });
+        } else if (exception instanceof QueryFailedError) {
+            res.emit('exception', {
+                status: Status.error,
+                message: exception.message,
+            });
+        } else if (exception instanceof EntityNotFoundError) {
+            res.emit('exception', {
+                status: Status.error,
+                message: exception.message,
+            });
+        }
+    }
+}
