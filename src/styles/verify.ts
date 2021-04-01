f9be4d169a19bd703f8e45a427beddc9fedadf75
winderica <winderica@gmail.com>
2021-04-01 13:44:05 +0800

    Update: improvements on UI

--- src/styles/verify.ts
+++ src/styles/verify.ts
@@ -1,23 +1,12 @@
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(({ breakpoints, spacing }) => ({
+const useStyles = makeStyles(({ spacing }) => ({
     content: {
-        minHeight: spacing(3),
-        maxWidth: 700,
         display: 'flex',
         alignItems: 'baseline',
     },
-    item: {
-        [breakpoints.down('xs')]: {
-            margin: spacing(0.5),
-        },
-        margin: spacing(1),
-    },
     input: {
-        [breakpoints.down('xs')]: {
-            width: 100,
-        },
-        width: 150,
+        marginLeft: spacing(1),
     },
 }));
 
