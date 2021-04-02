b7401a59b605526cedac5e44a5e7ef068df0c68d
winderica <winderica@gmail.com>
2021-04-03 00:19:31 +0800

    Fix: auto-fit -> auto-fill

--- src/styles/slots.ts
+++ src/styles/slots.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles(({ spacing }) => ({
     container: {
         display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 300px))',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
     },
     slotContainer: {
         display: 'flex',
