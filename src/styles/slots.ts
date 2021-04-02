6910f7e44a23c7e2ee96b105cb59f1aa902b5b89
winderica <winderica@gmail.com>
2021-04-02 22:43:35 +0800

    Fix: limit the max width of slot container

--- src/styles/slots.ts
+++ src/styles/slots.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles(({ spacing }) => ({
     container: {
         display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 300px))',
     },
     slotContainer: {
         display: 'flex',
