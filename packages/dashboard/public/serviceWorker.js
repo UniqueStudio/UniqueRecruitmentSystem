94cbdfe4fd8ad7b1274c86c8dbfa8b42200e2b0f
winderica <winderica@gmail.com>
2021-04-17 15:13:57 +0800

    Update: add checklist for ESM in SW

--- packages/dashboard/public/serviceWorker.js
+++ packages/dashboard/public/serviceWorker.js
@@ -1,6 +1,8 @@
 /*
  * TODO: import `workbox` directly when ES Module for service workers is supported by mainstream browsers.
- * See: https://bugs.chromium.org/p/chromium/issues/detail?id=824647
+ * Chrome: available in v91 (https://bugs.chromium.org/p/chromium/issues/detail?id=824647)
+ * Firefox: pending (https://bugzilla.mozilla.org/show_bug.cgi?id=1360870)
+ * Safari: available in TP122 (https://webkit.org/blog/11577/release-notes-for-safari-technology-preview-122/)
  */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
