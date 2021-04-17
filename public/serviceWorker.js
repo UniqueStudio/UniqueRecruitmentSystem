f8c458ce52366243b1f0081642ed527bd1c05ba9
winderica <winderica@gmail.com>
2021-04-17 14:49:55 +0800

    Fix: update workbox and it will no longer complain when failed to fetch index.html

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,9 +2,7 @@
  * TODO: import `workbox` directly when ES Module for service workers is supported by mainstream browsers.
  * See: https://bugs.chromium.org/p/chromium/issues/detail?id=824647
  */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
-
-self.__WB_DISABLE_DEV_LOGS = true;
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 const { recipes: { imageCache, pageCache, staticResourceCache } } = workbox;
 
