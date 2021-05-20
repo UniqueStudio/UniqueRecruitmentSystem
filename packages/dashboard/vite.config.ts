11ad4f32af2dcf5389ce9895bafe07003d624f6e
winderica <winderica@gmail.com>
2021-05-20 19:09:03 +0800

    Fix: fix indents

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -5,19 +5,20 @@ import { version } from './package.json';
 import { compilerOptions } from './tsconfig.json';
 
 export default defineConfig({
-  define: {
-    __APP_VERSION__: `"${version}"`,
-  },
-  resolve: {
-    alias: Object.fromEntries(
-      Object.entries(compilerOptions.paths).map(([key, value]) =>
-        [key.replace('*', ''), `/${value[0].replace('*', '')}`],
-      ),
-    ),
-  },
-  build: {
-    outDir: 'build',
-    minify: 'esbuild',
-  },
-  plugins: [refresh()],
+    define: {
+        __APP_VERSION__: `"${version}"`,
+    },
+    resolve: {
+        alias: Object.fromEntries(
+            Object.entries(compilerOptions.paths).map(([key, value]) => [
+                key.replace('*', ''),
+                `/${value[0].replace('*', '')}`,
+            ]),
+        ),
+    },
+    build: {
+        outDir: 'build',
+        minify: 'esbuild',
+    },
+    plugins: [refresh()],
 });
