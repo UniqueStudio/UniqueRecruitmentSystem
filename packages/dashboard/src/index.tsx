1b53f0256db4b713e1331964d162a4e12d4a46b5
winderica <winderica@gmail.com>
2021-05-21 15:16:04 +0800

    Update: register serviceworker in js rather than in html

--- packages/dashboard/src/index.tsx
+++ packages/dashboard/src/index.tsx
@@ -4,3 +4,11 @@ import { render } from 'react-dom';
 import { App } from '@views/App';
 
 render(<App />, document.getElementById('root'));
+
+// Now service worker only works under `/`, and cannot handle subroutes like `/dashboard` well.
+// One feasible solution is using `HashRouter` rather than `BrowserRouter`
+window.addEventListener('load', () => {
+    if (import.meta.env.PROD) {
+        navigator.serviceWorker?.register('/serviceWorker.js');
+    }
+});
