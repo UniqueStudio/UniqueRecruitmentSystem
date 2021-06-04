69359e3837fa8d93a1519d1b23c511d18eadb038
winderica <winderica@gmail.com>
2021-06-04 10:43:56 +0800

    Update: make `Modal` more generic

--- packages/dashboard/src/components/Modal/index.tsx
+++ packages/dashboard/src/components/Modal/index.tsx
@@ -1,36 +1,32 @@
-import { Modal as MuiModal, Slide, SlideProps, Typography } from '@material-ui/core';
+import { Modal as MuiModal, ModalTypeMap, Paper, Slide, SlideProps, Typography } from '@material-ui/core';
 import React, { FC, memo } from 'react';
 
 import useStyles from '@styles/modal';
 
-interface Props {
-    open: boolean;
+type Props = Exclude<ModalTypeMap['props'] & {
     direction?: SlideProps['direction'];
-    hideBackdrop?: boolean;
     title: string;
-    onClose?: () => void;
-}
+}, 'children'>;
 
-export const Modal: FC<Props> = memo(({ open, onClose, title, children, direction, hideBackdrop }) => {
+export const Modal: FC<Props> = memo(({ open, title, children, direction = 'right', ...other }) => {
     const classes = useStyles();
     const leaveDirection = direction === 'left' ? 'right' : 'left';
     return (
         <MuiModal
             open={open}
-            onClose={onClose}
             className={classes.modalContainer}
-            hideBackdrop={hideBackdrop}
             disableEnforceFocus
+            {...other}
         >
-            <Slide direction={open ? direction || 'right' : leaveDirection} in={open} mountOnEnter unmountOnExit>
-                <div className={classes.modal}>
+            <Slide direction={open ? direction : leaveDirection} in={open} mountOnEnter unmountOnExit>
+                <Paper className={classes.modal}>
                     <div className={classes.modalHeader}>
                         <Typography variant='h5' className={classes.modalTitle}>
                             {title}
                         </Typography>
                     </div>
                     {children}
-                </div>
+                </Paper>
             </Slide>
         </MuiModal>
     );
